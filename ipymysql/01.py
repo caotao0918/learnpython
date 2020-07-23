655f700094c85d4b32900d11903c06fb88609a65
caotao0918 <caotao0918@qq.com>
2020-07-23 14:42:27 +0800

    pymysql增删改查demo

--- ipymysql/01.py
+++ ipymysql/01.py
@@ -2,6 +2,8 @@
 import pymysql
 
 
+# 增删改查
+
 def main():
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456',
                            db='hrs', charset='utf8')
