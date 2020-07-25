77e69e8f6c3ed8b22060a750e02e152c80b2ab35
caotao0918 <caotao0918@qq.com>
2020-07-25 08:15:25 +0800

    pymysql增删改查demo

--- ipymysql/01.py
+++ ipymysql/01.py
@@ -10,13 +10,28 @@ def main():
     print(conn)
     try:
         with conn.cursor() as cursor:
-            cursor.execute('delete from tb_dept where dno=50')
+            res2 = cursor.execute('delete from tb_dept where dno=50')
+            if res2 == 1:
+                print('删除成功！！！')
+
             res = cursor.execute('insert into tb_dept values(50, "研发二部", "驻马店")')
-            cursor.execute('update tb_dept set dloc="南京" where dno=50')
-            res1 = cursor.execute('select * from tb_dept')
-            print(res1)
             if res == 1:
-                print('成功！')
+                print('插入成功！！！')
+
+            res3 = cursor.execute('update tb_dept set dloc="南京" where dno=50')
+            if res3 == 1:
+                print('修改成功！！！')
+            # 查询
+            res1 = cursor.execute('select dno, dname, dloc from tb_dept')
+            print('行数：', res1)
+            print(cursor.fetchmany(2))
+
+            # for row in cursor.fetchall():
+            #     print(f'部门编号：{row[0]}')
+            #     print(f'部门名称：{row[1]}')
+            #     print(f'部门地址：{row[2]}')
+            #     print('-------------')
+
             conn.commit()
     except pymysql.MySQLError as error:
         print(error)
